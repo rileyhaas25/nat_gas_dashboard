fc024376fc5cf7ebc278c40c9b6e255b14e17beb
rileyhaas25 <haasrk@g.cofc.edu>
2025-06-30 09:31:03 -0400

    update prices

--- page3.py
+++ page3.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 import plotly.graph_objects as go
 
 # update weekly here: https://rigcount.bakerhughes.com/na-rig-count
-rig_url = 'https://rigcount.bakerhughes.com/static-files/f0a24741-1be0-4fae-b071-a39fbafc7d01'
+rig_url = 'https://rigcount.bakerhughes.com/static-files/5d53fa56-38cb-4012-a244-d937576d494e'
 # update monthly here under Monthly U.S. dry shale natural gas production by formation: https://www.eia.gov/outlooks/steo/data.php
 production_url = "https://www.eia.gov/outlooks/steo/xls/Fig43.xlsx"
 FOCUS_BASINS = ["Marcellus", "Haynesville", "Permian", "Eagle Ford", "Utica", "Woodford"]
